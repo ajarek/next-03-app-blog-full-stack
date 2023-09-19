b4643a5389292ba6230a378fee674f677f6044b3
Ajarek <ajarek@poczta.onet.pl>
2023-09-19 22:18:15 +0200

    added Contact

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
-
+import Button from '@/components/Button/Button'
+import Image from 'next/image'
+import Photo from 'public/contact.png'
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='full-screen p-10'>
+      <h1 className={'text-3xl text-center'}>Bądźmy w kontakcie</h1>
+      <div className=' grid grid-cols-2 gap-10 mt-10'>
+        <div className='h-96  img-motion  flex justify-center items-center '>
+          
+          <Image
+            src={Photo}
+            alt=''
+            
+            className='img-color'
+          />
+        </div>
+        <div className=''>
+          <form className={'w-full flex flex-col'}>
+          <input type="text" placeholder="Imie  Nazwisko" className={'input'} />
+          <input type="text" placeholder="email" className={'input'} />
+          <textarea
+            className={'textarea'}
+            placeholder="wiadomość"
+            cols="30"
+            rows="8"
+          ></textarea>
+          <Button url="#" text="Wyślij"/>
+        </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const Button = ({ text, url }) => {
   return (
     <Link href={url}>
-      <button className='p-5 cursor-pointer bg-emerald-600 text-white rounded-md'>{text}</button>
+      <button className='px-5 py-3 cursor-pointer bg-emerald-600 text-white rounded-md'>{text}</button>
     </Link>
   )
 }
